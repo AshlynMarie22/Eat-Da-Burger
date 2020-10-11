e9f98e575a337863554bbcba4efcff1f6ba1dce9
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-10-11 00:09:00 -0400

    updated database

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,13 @@
 CREATE DATABASE burger_db;
 USE burger_db;
 
+DROP TABLE IF EXISTS burgers;
+
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
